24bbeb64ee0f13bb5f5e4e6ae78b272796e32b63
AnotherPillow <Thepilous_@outlook.com>
2023-06-08 18:38:58 +0100

    Update src_directory.py

--- src_directory.py
+++ src_directory.py
@@ -1,4 +1,6 @@
 import os
 if not os.path.exists('dist/Opal Overlay-win32-x64/resources/src'):
     os.makedirs('dist/Opal Overlay-win32-x64/resources/src')
-import shutil;shutil.copy('src/who.vbs', 'dist/Opal Overlay-win32-x64/resources/src/')
+import shutil;
+#copy all from src to dist
+shutil.copytree('src', 'dist/Opal Overlay-win32-x64/resources/src')
